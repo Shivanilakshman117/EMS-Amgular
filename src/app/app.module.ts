38a77d01aafc5cfd8d2e419c2a60a2b5ad3d7d4e
Shivani Lakshman <shivani.lakshman@psiog.com>
2019-10-24 17:53:05 +0530

    Login page front end is 60% done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from './core/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/core/material.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule, MatCardModule, MatDialogModule, MatInputModule, MatTableModule,
+  MatToolbarModule, MatMenuModule,MatIconModule, MatProgressSpinnerModule,MatCheckboxModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+  CommonModule, 
+  MatToolbarModule,
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatDialogModule,
+  MatTableModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCheckboxModule,
+  MatProgressSpinnerModule
+  ],
+  exports: [
+  CommonModule,
+   MatToolbarModule, 
+   MatButtonModule, 
+   MatCardModule, 
+   MatInputModule, 
+   MatDialogModule, 
+   MatTableModule, 
+   MatMenuModule,
+   MatIconModule,
+   MatCheckboxModule,
+   MatProgressSpinnerModule
+   ],
+})
+export class CustomMaterialModule { }
